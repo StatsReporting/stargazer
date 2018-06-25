b0c9c0add3b62fa6504a772844f5165cf2d6df6d
mwburke <matthew.wesley.burke@gmail.com>
2018-06-24 20:29:51 -0700

    version 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="stargazer",
-    version="0.0.1",
+    version="0.0.2",
     author="Matthew Burke",
     author_email="matthew.wesley.burke@gmail.com",
     description="Nicely formatted regression reporting",
