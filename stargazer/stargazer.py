4390a539a7fc85f1c7449b1bd2e5b5509e9274d7
Brandon Zborowski <brandonek15@gmail.com>
2020-04-13 08:31:13 -0500

    Made dynamic latex column code simpler

--- stargazer/stargazer.py
+++ stargazer/stargazer.py
@@ -442,12 +442,8 @@ class Stargazer:
 
             header += '  \\label{}\n'
 
-        latex_columns = 'l'
-
-        for i in range(self.num_models):
-        	latex_columns += 'c'
-
-        header += '\\begin{tabular}{@{\\extracolsep{5pt}}' + latex_columns + '}\n'
+        content_columns = 'c' * self.num_models
+        header += '\\begin{tabular}{@{\\extracolsep{5pt}}l' + content_columns + '}\n'
         header += '\\\\[-1.8ex]\\hline\n'
         header += '\\hline \\\\[-1.8ex]\n'
         if self.dep_var_name is not None:
