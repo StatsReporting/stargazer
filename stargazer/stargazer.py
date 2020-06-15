aee00d5b8be697a7d59189591607e2f898f22128
Max Ghenis <mghenis@gmail.com>
2020-06-15 15:21:06 -0700

    Remove unused and commented code

--- stargazer/stargazer.py
+++ stargazer/stargazer.py
@@ -10,7 +10,6 @@ https://CRAN.R-project.org/package=stargazer
         github.com/mwburke
 """
 
-from __future__ import print_function
 from statsmodels.regression.linear_model import RegressionResultsWrapper
 from math import sqrt
 
@@ -658,11 +657,3 @@ class Stargazer:
 
     def _repr_html_(self):
         return self.render_html()
-
-    # Begin Markdown render functions
-    # def render_markdown(self):
-    #     print("sorry haven't made this yet :/")
-
-    # Begin ASCII render functions
-    # def render_ascii(self):
-    #     print("sorry haven't made this yet :/")
