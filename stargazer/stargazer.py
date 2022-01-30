57452b8177a017c0b879b4e14088ffb17567a7fe
leogoldman <leogoldman@Leos-MBP-2.lan>
2022-01-30 13:27:05 -0500

    Added same minor fix in HTML rendering

--- stargazer/stargazer.py
+++ stargazer/stargazer.py
@@ -592,7 +592,7 @@ class HTMLRenderer(Renderer):
         for i, note in enumerate(self.custom_notes):
             if (i != 0) | (self.notes_append):
                 notes_text += '<tr>'
-            notes_text += '<td></td><td colspan="' + str(self.num_models) + '" style="text-align: right">' + note + '</td></tr>'
+            notes_text += '<td colspan="' + str(self.num_models+1) + '" style="text-align: right">' + note + '</td></tr>'
 
         return notes_text
 
